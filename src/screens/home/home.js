6bcac6585870a930d9e25a0f1a51250937d5002f
Axim10 <32218932+Axim10@users.noreply.github.com>
2019-03-14 21:32:35 +0530

    Update home.js

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,18 +1,85 @@
 import React, { Component } from 'react';
-import './home.css';
+import './Home.css';
 import Header from '../../common/header/Header';
-import { Redirect } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import {GridList, GridListTile} from '@material-ui/core';
-import axios from "axios"
-import GridListCard from './../../components/GridListCard';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import indigo from '@material-ui/core/colors/indigo';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const styles = theme => ({
+    cardsGridList: {
+        width: '80%',
+    },
+    card: {
+        maxWidth: 500,
+        marginTop: 20,
+        marginBottom: 10,
+        marginLeft: 5,
+    },
+    cardHeaderTitle: {
+        fontWeight: 'bold',
+    },
+    cardHeaderSubheader: {
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    cardImage: {
+        height: '100%',
+        width: '100%',
+    },
+    caption: {
+        fontWeight: 'bold',
+    },
+    tags: {
+        color: '#82C0FF',
+    },
+    favoriteIconGridItem: {
+        marginTop: 5,
+    },
+    likesCount: {
+        marginTop: '10%',
+        fontWeight: 'bold',
+        marginLeft: 5,
+    },
+    commentForm: {
+        width: '80%',
+    },
+    commentLabel: {
+        '&$commentFocused': {
+            color: indigo[500],
+        },
+    },
+    commentFocused: {},
+    commentInputUnderline: {
+        '&:after': {
+            borderBottomColor: indigo[500],
+        },
+    },
+    commentButton: {
+        marginTop: 15,
+        marginLeft: 10,
+    },
+});
 
 class Home extends Component {
 
     constructor() {
         super();
-        localStorage.setItem('access-token', '11222840801.f3fe1ea.54bee85b35d045aabcc9580f743c0dc2');
-        // sessionStorage.removeItem('access-token');
         this.state = {
             userPosts: [],
             userInfo: {},
@@ -27,23 +94,29 @@ class Home extends Component {
 
         // Get information about the owner of the access_token.
         let dataUserInfo = null;
-       
-        axios.get(`${this.props.baseUrl}?access_token=${localStorage.getItem('access-token')}`).then((response)=>{
-         
-            that.setState({
-                userInfo: response.data.data
-            })
+        let xhrUserInfo = new XMLHttpRequest();
+        xhrUserInfo.addEventListener('readystatechange', function () {
+            if (this.readyState === 4) {
+                that.setState({
+                    userInfo: JSON.parse(this.responseText).data
+                })
+            }
         })
 
+        xhrUserInfo.open('GET', `${this.props.userInfoUrl}${this.props.accessToken}`);
+        xhrUserInfo.send(dataUserInfo);
+
+        // Get the most recent media published by the owner of the access_token.
         let dataUserPosts = null;
-    
-        axios.get(`${this.props.baseUrl}media/recent/?access_token=${localStorage.getItem('access-token')}`).then((response)=>{
-            let likesState = {};
+        let xhrUserPosts = new XMLHttpRequest();
+        xhrUserPosts.addEventListener('readystatechange', function () {
+            if (this.readyState === 4) {
+                let likesState = {};
                 let userComments = {};
-                let data = response.data.data
+                let data = JSON.parse(this.responseText).data;
                 for (let i = 0; i < data.length; i++) {
                     likesState[data[i]['id']] = false
-                    userComments[data[i]['id']] = {'added': [], 'toAdd': ''}
+                    userComments[data[i]['id']] = { 'added': [], 'toAdd': '' }
                 }
                 that.setState({
                     userPosts: data,
@@ -51,12 +124,17 @@ class Home extends Component {
                     likesState: likesState,
                     userComments: userComments
                 });
-        })
+            }
+        });
+
+        xhrUserPosts.open('GET', `${this.props.userMediaRecentUrl}${this.props.accessToken}`);
+        xhrUserPosts.send(dataUserPosts);
     }
 
-    timeReshape(unixTimestamp) {
+    // takes unix timestamp and returns date string in 'dd/mm/yyyy HH:MM:SS' format
+    prettyTimestamp(unixTimestamp) {
 
-        // created_time which is unix timestamp multiplied by 1000 so that the time is in milliseconds, not seconds
+        // created_time which is a unix timestamp multiplied by 1000 so that the time is in milliseconds, not seconds
         let dateObject = new Date(unixTimestamp * 1000);
         let timeDict = {
             date: dateObject.getDate(),
@@ -76,7 +154,7 @@ class Home extends Component {
         return `${timeDict.date}/${timeDict.month}/${timeDict.year} ${timeDict.hours}:${timeDict.minutes}:${timeDict.hours}`;
     }
 
-    goToProfile = () => {
+    myAccountHandler = () => {
         // redirect to profile page
         this.props.history.push('/profile');
     }
@@ -91,24 +169,22 @@ class Home extends Component {
 
     searchHandler = (event) => {
         let resultPosts = this.state.userPosts.filter(function (post) {
-            return post.caption!==null?post.caption.text.split('\n')[0].toLowerCase().includes(event.target.value.toLowerCase()):null;
+            return post.caption.text.split('\n')[0].toLowerCase().includes(event.target.value.toLowerCase());
         });
         this.setState({ filteredUserPosts: resultPosts });
     }
 
-    toggleLike(postId, likeState) {
+    toggleLikeCount(postId, likeState) {
         let newUserPosts = Object.assign({}, this.state.userPosts);
         let count = null;
         for (let i = 0; i < Object.keys(newUserPosts).length; i++) {
             if (newUserPosts[i]['id'] === postId) {
                 if (likeState) {
-                    count = newUserPosts[i].likes.count + 1
+                    count = newUserPosts[i].likes.count + 1;
                     newUserPosts[i].likes.count = count;
-                    // console.log(newUserPosts[i].likes.count)
                 } else {
                     count = newUserPosts[i].likes.count - 1;
                     newUserPosts[i].likes.count = count;
-                    // console.log(newUserPosts[i].likes.count)
                 }
                 break;
             }
@@ -118,7 +194,6 @@ class Home extends Component {
         for (let i = 0; i < Object.keys(newFilteredUserPosts).length; i++) {
             if (newFilteredUserPosts[i]['id'] === postId) {
                 newFilteredUserPosts[i].likes.count = count;
-                // console.log(count)
                 break;
             }
         }
@@ -135,78 +210,153 @@ class Home extends Component {
     likeHandler = (postId) => {
         if (this.state.likesState[postId]) {
             // decrement like
-            this.toggleLike(postId, false);
+            this.toggleLikeCount(postId, false);
         }
         else {
             // increment like
-            this.toggleLike(postId, true);
+            this.toggleLikeCount(postId, true);
         }
     }
 
-    commentInputChange = (userComment, postId) => {
+    commentInputChangeHandler = (userComment, postId) => {
         let newUserComments = Object.assign({}, this.state.userComments);
         newUserComments[postId]['toAdd'] = userComment;
-        this.setState({userComments: newUserComments});
+        this.setState({ userComments: newUserComments });
     }
 
-    addComment = (id) => {
-        if (this.state.userComments[id]['toAdd']) {
+    addCommentHandler = (postId) => {
+        if (this.state.userComments[postId]['toAdd']) {
             let newUserComments = Object.assign({}, this.state.userComments);
-            newUserComments[id]['added'].push(newUserComments[id]['toAdd']);
-            newUserComments[id]['toAdd'] = ''
-            this.setState({userComments: newUserComments});
+            newUserComments[postId]['added'].push(newUserComments[postId]['toAdd']);
+            newUserComments[postId]['toAdd'] = '';
+            this.setState({ userComments: newUserComments });
         }
     }
 
     render() {
-        if (localStorage.getItem('access-token') !== null) {
-        
         const { classes } = this.props;
-
         return (
             <div>
+
+                {/* header */}
                 <Header
-                    myAccountHandler={this.goToProfile}
-                    profilePictureUrl={this.state.userInfo.profile_picture}
-                    baseUrl={this.props.baseUrl}
-                    showSearchBox='true'
-                    showProfilePicture='true'
-                    showMyAccountMenu='true'
+                    showSearchBox={true}
+                    showProfilePicture={true}
+                    showMyAccountMenu={true}
+                    myAccountHandler={this.myAccountHandler}
                     logoutHandler={this.logoutHandler}
                     searchHandler={this.searchHandler}
+                    profilePictureUrl={this.state.userInfo.profile_picture}
                 />
+
                 <div id='cards-grid-list'>
-                    <GridList cols={2} cellHeight='auto' className={classes.cardsGrid}>
-                        {this.state.filteredUserPosts.map((filteredPost,index) => (
-                            <GridListTile key={index} >
-                                <GridListCard  
-                                    filteredPost={filteredPost} 
-                                    timeReshape={this.timeReshape}
-                                    likeHandler={this.likeHandler}
-                                    likeState={this.state.likesState[filteredPost.id]}
-                                    userComments={this.state.userComments[filteredPost.id]['added']}
-                                    userComment={this.state.userComments[filteredPost.id]['toAdd']}
-                                    commentInputChange={this.commentInputChange}
-                                    addComment={this.addComment}
+                    <GridList cols={2} cellHeight='auto' className={classes.cardsGridList}>
+
+                        {/* list all the filtered posts */}
+                        {this.state.filteredUserPosts.map(post => (
+                            <GridListTile key={'post' + post.id}>
+
+                                {/* card for image post */}
+                                <Card className={classes.card}>
+                                    <CardHeader
+                                        classes={{
+                                            title: classes.cardHeaderTitle,
+                                            subheader: classes.cardHeaderSubheader,
+                                        }}
+
+                                        // card header - user profile picture
+                                        avatar={
+                                            <Avatar alt='Profile picture' src={post.user.profile_picture} />
+                                        }
+
+                                        // card header - username
+                                        title={post.user.username}
+
+                                        // card header - created date
+                                        subheader={this.prettyTimestamp(post.created_time)}
                                     />
+                                    <CardContent>
+
+                                        {/* card content - image */}
+                                        <img className={classes.cardImage} src={post.images.standard_resolution.url} alt={post.caption.text} />
+
+                                        {/* card content - horizontal rule */}
+                                        <hr />
+
+                                        {/* card content - caption of the image*/}
+                                        <Typography className={classes.caption} variant='subtitle1'>
+                                            {post.caption.text.split('\n')[0]}
+                                        </Typography>
+
+                                        {/* card content - hashtags */}
+                                        <Typography className={classes.tags} variant='subtitle2'>
+                                            {post.tags.map(function (t) { return `#${t} ` })}
+                                        </Typography>
+
+                                        {/* card content - like icon and number of likes */}
+                                        <Grid container={true} direction='row' alignItems='center'>
+                                            <Grid item={true} className={classes.favoriteIconGridItem}>
+                                                <IconButton onClick={() => this.likeHandler(post.id)}>
+                                                    {this.state.likesState[post.id] ?
+                                                        <FavoriteIcon nativeColor='red' fontSize='large' /> :
+                                                        <FavoriteBorderIcon nativeColor='black' fontSize='large' />
+                                                    }
+                                                </IconButton>
+                                            </Grid>
+                                            <Grid item={true}>
+                                                <Typography className={classes.likesCount} variant='body2'>
+                                                    {post.likes.count} likes
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+
+                                        {/* card content - comments */}
+                                        <List style={{ marginTop: '-5%' }}>
+                                            {this.state.userComments[post.id]['added'].map((userComment, index) => (
+                                                <ListItem key={post.id + 'comment' + index} style={{ marginBottom: '-5%' }}>
+                                                    <Typography variant='body1' style={{ fontWeight: 'bold' }}>{post.user.username}:</Typography>
+                                                    <Typography variant='subtitle1' style={{ marginLeft: 5 }}>{userComment}</Typography>
+                                                </ListItem>
+                                            ))}
+                                        </List>
+
+                                        {/* card content - add comment */}
+                                        <div id='comment-div'>
+                                            <FormControl className={classes.commentForm}>
+                                                <InputLabel
+                                                    classes={{
+                                                        root: classes.commentLabel,
+                                                        focused: classes.commentFocused,
+                                                    }}
+                                                >
+                                                    Add a comment
+                                                </InputLabel>
+                                                <Input
+                                                    classes={{
+                                                        underline: classes.commentInputUnderline,
+                                                    }}
+                                                    value={this.state.userComments[post.id]['toAdd']}
+                                                    onChange={(event) => this.commentInputChangeHandler(event.target.value, post.id)}
+                                                />
+                                            </FormControl>
+                                            <Button
+                                                className={classes.commentButton}
+                                                variant='contained'
+                                                color='primary'
+                                                onClick={() => this.addCommentHandler(post.id)}
+                                            >
+                                                ADD
+                                            </Button>
+                                        </div>
+                                    </CardContent>
+                                </Card>
                             </GridListTile>
                         ))}
                     </GridList>
                 </div>
-
             </div>
-        )}
-        else{
-            return <Redirect to="/" />
-        }
-       
+        )
     }
 }
 
-const styles = theme => ({
-    cardsGrid: {
-        width: '80%',
-    },
-    
-});
 export default withStyles(styles)(Home);
