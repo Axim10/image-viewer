700c5e41d2bb4d08664feb95a826dcdf8d19a7db
Axim10 <32218932+Axim10@users.noreply.github.com>
2019-03-15 06:50:57 +0530

    Errors Cleared

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -11,43 +11,41 @@ import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
- const styles = theme => ({
-    
-        card1: {
-            maxWidth: '30%',
-             marginTop: 18,
-            margin: 'auto',
-           
-            padding: 50,
+const styles = theme => ({
+
+    card1: {
+        maxWidth: '30%',
+        marginTop: 18,
+        margin: 'auto',
+
+        padding: 50,
+    },
+
+
+    inputLabel: {
+        '&$inputFocused': {
+            color: indigo[500],
         },
-       
-       
-        inputLabel: {
-            '&$inputFocused': {
-                color: indigo[500],
-            },
+    },
+    logFm: {
+        width: '100%',
+        marginTop: 25,
+    },
+    inputFocused: {},
+    inputUnderline: {
+        '&:after': {
+            borderBottomColor: indigo[500],
         },
-        logFm: {
-            width: '100%',
-            marginTop: 25,
-        },
-        inputFocused: {},
-        inputUnderline: {
-            '&:after': {
-                borderBottomColor: indigo[500],
-            },
-        },
-        
-    });
+    },
+
+});
 
 class Login extends Component {
     constructor() {
         super();
-       // sessionStorage.setItem('access-token', '11222840801.f3fe1ea.54bee85b35d045aabcc9580f743c0dc2');
-        // sessionStorage.removeItem('access-token');
+
         this.state = {
-        //    Username: 'screw_you_thanos',
-       //     Password: 'thaniosgay',
+
             logUname: '',
             usernameRequired: false,
             logPwd: '',
@@ -58,26 +56,26 @@ class Login extends Component {
 
 
     inputUsernameChangeHandler = (event) => {
-        this.setState({logUname: event.target.value})
+        this.setState({ logUname: event.target.value })
     }
 
     inputPasswordChangeHandler = (event) => {
-        this.setState({logPwd: event.target.value})
+        this.setState({ logPwd: event.target.value })
     }
 
     loginClickHandler = () => {
-        this.state.logUname === '' ? this.setState({usernameRequired: true}) : this.setState({usernameRequired: false});
-        this.state.logPwd === '' ? this.setState({passwordRequired: true}) : this.setState({passwordRequired: false});
+        this.state.logUname === '' ? this.setState({ usernameRequired: true }) : this.setState({ usernameRequired: false });
+        this.state.logPwd === '' ? this.setState({ passwordRequired: true }) : this.setState({ passwordRequired: false });
 
         if (this.state.logUname && this.state.logPwd) {
-            let username ='screw_you_thanos';
-            let password = 'thaniosgay';
+            let username = 'admin';
+            let password = 'admin';
             let accessToken = '11222840801.f3fe1ea.54bee85b35d045aabcc9580f743c0dc2';
             if (username === this.state.logUname && password === this.state.logPwd) {
                 sessionStorage.setItem('access-token', accessToken);
                 this.props.history.push('/home');
             } else {
-                this.setState({incorrectCredentials: true})
+                this.setState({ incorrectCredentials: true })
             }
         }
     }
@@ -85,11 +83,11 @@ class Login extends Component {
     render() {
 
         const { classes } = this.props;
-        
+
         return (
             <div>
 
-             
+
                 <Header />
                 <Card className={classes.card1}>
                     <CardContent>
@@ -123,35 +121,35 @@ class Login extends Component {
                             }
                         </FormControl>
 
-                     
+
                         <form>
-                        <FormControl required className={classes.logFm}>
-                            <InputLabel
-                                htmlFor='logPwd'
-                                classes={{
-                                    root: classes.inputLabel,
-                                    focused: classes.inputFocused,
-                                }}
-                            >
-                                Password
+                            <FormControl required className={classes.logFm}>
+                                <InputLabel
+                                    htmlFor='logPwd'
+                                    classes={{
+                                        root: classes.inputLabel,
+                                        focused: classes.inputFocused,
+                                    }}
+                                >
+                                    Password
                             </InputLabel>
-                            <Input
-                                autoComplete='off'
-                                id='loginPassword'
-                                type='password'
-                                loginpassword={this.state.logPwd}
-                                classes={{
-                                    underline: classes.inputUnderline,
-                                }}
-                                onChange={this.inputPasswordChangeHandler}
-                            />
-                            {this.state.passwordRequired ?
-                                <FormHelperText error={true}>
-                                    <span className='red'>required</span>
-                                </FormHelperText>
-                                : ''
-                            }
-                        </FormControl>
+                                <Input
+                                    autoComplete='off'
+                                    id='loginPassword'
+                                    type='password'
+                                    loginpassword={this.state.logPwd}
+                                    classes={{
+                                        underline: classes.inputUnderline,
+                                    }}
+                                    onChange={this.inputPasswordChangeHandler}
+                                />
+                                {this.state.passwordRequired ?
+                                    <FormHelperText error={true}>
+                                        <span className='red'>required</span>
+                                    </FormHelperText>
+                                    : ''
+                                }
+                            </FormControl>
                         </form>
 
                         {this.state.incorrectCredentials ?
