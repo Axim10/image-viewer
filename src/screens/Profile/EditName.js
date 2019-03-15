1984de6e5ded2d49b297d634e661a733c78eba43
Axim10 <32218932+Axim10@users.noreply.github.com>
2019-03-15 07:21:14 +0530

    Removed  unused vars

--- src/screens/Profile/EditName.js
+++ src/screens/Profile/EditName.js
@@ -4,10 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Fab from '@material-ui/core/Fab';
-import Icon from '@material-ui/core/Icon';
 
 export default class EditName extends React.Component {
   state = {
